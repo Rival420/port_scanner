8b9193f79a729b1c00110ea62ada7ee532d494e3
Rival420 <51548322+Rival420@users.noreply.github.com>
2020-01-14 12:54:21 +0000

    Create port_scanner.py
    V1.0

---
+++ port_scanner.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import socket
+import sys
+from datetime import datetime
+
+#Define Target
+if len(sys.argv) == 2:
+	target = socket.gethostbyname(sys.argv[1]) #translating hostname to IPV4
+else:
+	print("Invalid amount of arguments.")
+	print("Syntax: python3 port_scanner.py <ip>")
+	sys.exit()
+
+#Add Banner
+print("-" * 50)
+print("Scanning target " + target)
+print("Time started: " + str(datetime.now()))
+print("-" * 50)
+
+try:
+	for port in range(21,139):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		result = s.connect_ex((target,port)) #returns an error indicator
+		if result == 0:
+			print("Port {} is open".format(port))
+		s.close()
+
+except KeyboardInterrupt: 
+	print("\nExiting program.")
+	sys.exit()
+
+except socket.gaierror:
+	print("Hostname could not be resolved")
+	sys.exit()
+
+except socket.error:
+	print("Couldn't connect to server.")
+	sys.exit()
